50c14873236f61a5742fcf7bac736908d01007f3
Kristen Steele <ksteelephoto@gmail.com>
2021-02-26 15:11:20 -0600

    Provide a query that shows total sales made by each sales agent.

---
+++ sales_agent_total_sales.sql
@@ -0,0 +1,10 @@
+-- Provide a query that shows total sales 
+-- made by each sales agent.
+
+SELECT
+e.FirstName || ' ' || e.LastName AS Sales_Agent,
+ROUND(TOTAL(i.total), 2) Total_Sales
+FROM Invoice i, Customer c, Employee e
+WHERE i.customerId = c.CustomerId
+    AND c.SupportRepId = e.EmployeeId
+GROUP BY e.EmployeeId;
