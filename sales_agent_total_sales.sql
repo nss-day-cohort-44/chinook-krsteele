4945c61a5a2e9dd15846d72cdbccaf529f3b51f9
Kristen Steele <ksteelephoto@gmail.com>
2021-02-28 19:13:53 -0600

    added dollar sign

--- sales_agent_total_sales.sql
+++ sales_agent_total_sales.sql
@@ -3,7 +3,7 @@
 
 SELECT
 e.FirstName || ' ' || e.LastName AS Sales_Agent,
-ROUND(TOTAL(i.total), 2) Total_Sales
+"$" || ROUND(TOTAL(i.total), 2) Total_Sales
 FROM Invoice i, Customer c, Employee e
 WHERE i.customerId = c.CustomerId
     AND c.SupportRepId = e.EmployeeId
